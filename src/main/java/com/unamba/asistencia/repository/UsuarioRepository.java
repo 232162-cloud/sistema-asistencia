30e2b26274f1ba0ea5b985fe414a20c08e9e05cd
Jhojan Castro Curi <232162@unamba.edu.pe>
2026-06-29 12:03:48 -0500

    Agrega entidad Usuario, repositorio y actualiza asistencias

---
+++ src/main/java/com/unamba/asistencia/repository/UsuarioRepository.java
@@ -0,0 +1,12 @@
+package com.unamba.asistencia.repository;
+
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.unamba.asistencia.model.Usuario;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    Optional<Usuario> findByUsername(String username);
+
+}
