7e26873104541a61c0d164f6cb0e45e196daafa7
Jhojan Castro Curi <232162@unamba.edu.pe>
2026-07-14 08:46:44 -0500

    Agrega dashboard principal

---
+++ sistema-asistencia-frontend/src/app/features/dashboard/dashboard.component.ts
@@ -0,0 +1,15 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { AuthService } from '../../core/services/auth.service';
+
+@Component({
+  selector: 'app-dashboard',
+  standalone: true,
+  imports: [CommonModule, RouterLink],
+  templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.scss'
+})
+export class DashboardComponent {
+  constructor(public auth: AuthService) {}
+}
