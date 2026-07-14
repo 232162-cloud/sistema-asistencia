db8e7daee381e58e7b03e66da9ea7d3c57715a86
Jhojan Castro Curi <232162@unamba.edu.pe>
2026-07-14 08:48:53 -0500

    Agrega componentes compartidos y barra de navegacion

---
+++ sistema-asistencia-frontend/src/app/shared/components/navbar/navbar.component.ts
@@ -0,0 +1,20 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { AuthService } from '../../../core/services/auth.service';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [CommonModule, RouterLink, RouterLinkActive],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.scss'
+})
+export class NavbarComponent {
+  constructor(public auth: AuthService, private router: Router) {}
+
+  cerrarSesion(): void {
+    this.auth.logout();
+    this.router.navigate(['/login']);
+  }
+}
